0e1fc334b7bb2e5b40f0189f0f5c7cc24f64592c
bool_ <asnavely1994@gmail.com>
2020-03-26 15:35:07 -0700

    resources api parsing code

--- src/components/plex_device_selection.tsx
+++ src/components/plex_device_selection.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import { PlexOptions, Resource } from '../plex';
-import * as plex from '../plex'
+import * as plex from '../plex';
 
 interface PlexDeviceSelectionProps {
   plexOptions: PlexOptions;
+
   onDeviceSelection(resource: Resource): any;
 }
 
-export class PlexDeviceSelection extends React.Component<PlexDeviceSelectionProps> {
+interface PlexDeviceSelectionState {
+  devices: Resource[];
+}
+
+export class PlexDeviceSelection extends React.Component<PlexDeviceSelectionProps, PlexDeviceSelectionState> {
+  constructor(props: PlexDeviceSelectionProps) {
+    super(props);
+    this.state = {
+      devices: [],
+    };
+  }
 
   async componentDidMount() {
     const client = plex.client(this.props.plexOptions);
     const resources: Resource[] = await plex.resources(client);
-    console.log(resources);
+    resources.forEach((resource) => {
+        console.log(resource);
+        // filter out anything that doesn't include player or client
+        if (resource.provides.includes('client') || resource.provides.includes('player') ) {
+          this.setState(state => {
+            const list = [...state.devices, resource];
+            return {
+              devices: list
+            };
+          });
+        }
+      },
+    );
   }
 
   componentWillUnmount() {
@@ -21,8 +44,14 @@ export class PlexDeviceSelection extends React.Component<PlexDeviceSelectionProp
   render() {
     return (
       <div>
-        Just keep swimming...
+        <ul>
+          {
+            this.state.devices.map((resource) => (
+              <li key={resource.clientIdentifier}>{resource.name}</li>
+            ))
+          }
+        </ul>
       </div>
-    )
+    );
   }
 }
